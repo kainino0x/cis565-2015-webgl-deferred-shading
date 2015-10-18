cef861d310fe03657fd286028a38b66604f92c2a
Kai Ninomiya <kainino1@gmail.com>
2015-10-18 18:35:12 -0400

    ambient light

--- glsl/deferred.frag.glsl
+++ glsl/deferred.frag.glsl
@@ -17,8 +17,8 @@ vec3 applyNormalMap(vec3 geomnor, vec3 normap) {
     normap = normap * 2.0 - 1.0;
     vec3 up = normalize(vec3(0.001, 1, 0.001));
     vec3 surftan = normalize(cross(geomnor, up));
-    vec3 surfbinor = normalize(cross(geomnor, surftan));
-    return normalize(normap.y * surftan + normap.x * surfbinor + normap.z * geomnor);
+    vec3 surfbinor = cross(geomnor, surftan);
+    return normap.y * surftan + normap.x * surfbinor + normap.z * geomnor;
 }
 
 void main() {
@@ -29,7 +29,7 @@ void main() {
     float depth = texture2D(u_depth, v_uv).x;
 
     vec3 pos = gb0.xyz;
-    vec3 geomnor = gb1.xyz;
+    vec3 geomnor = normalize(gb1.xyz);
     vec3 colmap = gb2.xyz;
     vec3 normap = gb3.xyz;
 
@@ -62,5 +62,5 @@ void main() {
         vec3 lightdir = lightdiff / lightdist;
         diff += vec3(5.0) * max(0.0, dot(nor, lightdir)) / lightdist;
     }
-    gl_FragColor = vec4(colmap * diff, 1.0);
+    gl_FragColor = vec4(colmap * (vec3(0.2) + diff), 1.0);
 }
